c427d5d31f3d6ad474c7d46fec2cc6d0b15df107
Wayne <w.p.lam107@gmail.com>
2021-02-15 21:47:34 -0500

    initial app functions folder

--- app/plots.js
+++ app/plots.js
@@ -1,7 +1,7 @@
 let labels = ["ADD LATER"];
 let datasetsLabel = "ADD LATER";
 let backgroundColor = ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"];
-let data = []
+let data = [];
 
 new Chart(document.getElementById('myChart'), {
     type: "bar",
