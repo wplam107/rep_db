7ed6b2d7e8abb956ae5ff1c87309837c0e68c88e
Wayne <w.p.lam107@gmail.com>
2021-02-15 19:40:13 -0500

    removed python frontend, added JS frontend

---
+++ app/plots.js
@@ -0,0 +1,26 @@
+let labels = ["ADD LATER"];
+let datasetsLabel = "ADD LATER";
+let backgroundColor = ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"];
+let data = []
+
+new Chart(document.getElementById('myChart'), {
+    type: "bar",
+    data: {
+        labels: labels,
+        datasets: [
+            {
+                label: "Population (millions)",
+                backgroundColor: backgroundColor,
+                data: data
+            }
+        ]
+    },
+    options: {
+        legend: { display: false },
+        title: {
+            display: true,
+            text: "US Representatives (X by Y)"
+        },
+        responsive: false
+    }
+});
