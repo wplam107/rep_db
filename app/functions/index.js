64c6126966cc3f4c05b11a5254ceb0d9fe32a192
Wayne <w.p.lam107@gmail.com>
2021-02-16 23:02:21 -0500

    check

--- app/functions/index.js
+++ app/functions/index.js
@@ -1,9 +1,22 @@
-const functions = require("firebase-functions");
+// const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const serviceAccount = require(
+    "./auth/rep-database-firebase-adminsdk-5mjsx-1632bf8f78.json"
+);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+const repsRef = db.collection("reps");
+
+const docsSnapshot = repsRef.limit(3).get();
+const repsData = docsSnapshot.map(doc => {
+    doc.data();
+});
+
+repsData.forEach(rep => {
+    console.log(rep);
+});
