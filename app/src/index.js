a5ba563221f2e013c65dc102649604d504f87cac
Wayne <w.p.lam107@gmail.com>
2021-02-21 14:12:24 -0500

    db to frontend

--- app/src/index.js
+++ app/src/index.js
@@ -1,26 +1,26 @@
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-
-// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
-// import * as firebase from "firebase/app"
-
-// If you enabled Analytics in your project, add the Firebase SDK for Analytics
-// import "firebase/analytics";
-
-// Add the Firebase products that you want to use
-// import "firebase/auth";
-
-
-// TODO: Replace the following with your app's Firebase project configuration
-// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-
-
-// Initialize Firebase
 import { _firebase } from "./database.js"
 
 const db = _firebase.firestore();
 
-db.collection("reps").limit(3).get().then((querySnaphot) => {
-    querySnaphot.forEach((doc) => {
+window._data = [];
+
+async function getReps() {
+    const snapshot = await db.collection("reps").limit(3).get();
+    snapshot.docs.forEach((doc) => {
+        _data.push(doc.data());
         console.log(doc.data());
     });
-});
+};
+
+async function newText() {
+    const result = await getReps();
+    for (const [key, value] of Object.entries(_data[0])) {
+        var body = document.querySelector("body");
+        var text = document.createElement("div");
+        var string = `${key}: ${key == "dob" ? new Date(value['seconds']*1000): value}`;
+        text.innerText = string;
+        body.appendChild(text);
+    };
+};
+
+newText();
