30dc95f2b3eaa969fd544aceb96cfe0f4aa96f7f
Wayne <w.p.lam107@gmail.com>
2021-02-22 22:34:50 -0500

    adding D3 plots

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    database: ['firebase/app', 'firebase/firestore'],
+    database: './src/database.js',
     index: {
       import: './src/index.js',
-      dependOn: 'database',
+      dependOn: ['database'],
     },
   },
   output: {
